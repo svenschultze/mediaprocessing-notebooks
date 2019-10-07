0ec27df312adcdcc0a47613ef009d3e718c336c6
Sven Schultze <sven.schultze@uol.de>
2019-10-07 16:25:22 +0200

    Add image download function

--- colabcam.py
+++ colabcam.py
@@ -3,6 +3,7 @@ import numpy as np
 import cv2
 from IPython.display import Javascript
 from google.colab.output import eval_js
+from skimage import io
 
 def take_photo(filename='photo.jpg', quality=0.8):
   js = Javascript('''
@@ -37,3 +38,6 @@ def take_photo(filename='photo.jpg', quality=0.8):
   image = cv2.imdecode(image, cv2.IMREAD_COLOR)
   # return the image
   return image
+
+def download_image(url):
+  return io.imread(url)
